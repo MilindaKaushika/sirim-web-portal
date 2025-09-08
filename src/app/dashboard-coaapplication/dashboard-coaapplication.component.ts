5d7f7073ddd2e70aae2b8f33cc5317b81e26bd45
Milinda Kaushika <milinda.kaushika@scicom.com.my>
2025-09-08 08:29:59 +0530

    add changes

--- src/app/dashboard-coaapplication/dashboard-coaapplication.component.ts
+++ src/app/dashboard-coaapplication/dashboard-coaapplication.component.ts
@@ -8,12 +8,15 @@ import {DomSanitizer} from "@angular/platform-browser";
   styleUrls: ['./dashboard-coaapplication.component.css']
 })
 export class DashboardCoaapplicationComponent implements OnInit {
-
+  isDisabled = false;
   constructor(private iconRegistry: MatIconRegistry,
               private sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
       'calendar',
       sanitizer.bypassSecurityTrustResourceUrl('assets/icons/calendar.svg'));
+    iconRegistry.addSvgIcon(
+      'clock',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/clock.svg'));
   }
 
   ngOnInit(): void {
