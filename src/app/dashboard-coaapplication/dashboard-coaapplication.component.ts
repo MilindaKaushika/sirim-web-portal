72b9d8fd9e456f71d320d031b3e91cce8e43eb25
Milinda Kaushika <milinda.kaushika@scicom.com.my>
2025-09-08 09:10:45 +0530

    add changes

--- src/app/dashboard-coaapplication/dashboard-coaapplication.component.ts
+++ src/app/dashboard-coaapplication/dashboard-coaapplication.component.ts
@@ -1,6 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatIconRegistry} from "@angular/material/icon";
 import {DomSanitizer} from "@angular/platform-browser";
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    name: 'Hydrogen gdfgdfgdfgfdg gfdgdfgd',
+    weight: 1.0079,
+    symbol: 'H',
+  },
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 
 @Component({
   selector: 'app-dashboard-coaapplication',
@@ -19,7 +48,33 @@ export class DashboardCoaapplicationComponent implements OnInit {
       sanitizer.bypassSecurityTrustResourceUrl('assets/icons/clock.svg'));
   }
 
+
+  displayedColumns: string[] = [
+    'select',
+    'position',
+    'name',
+    'weight',
+    'symbol',
+    'actions',
+  ];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+  @ViewChild(MatSort, {static: true}) sort!: MatSort;
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
   ngOnInit(): void {
+    this.dataSource.sort = this.sort;
   }
 
 }
