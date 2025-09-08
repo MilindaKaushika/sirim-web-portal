5900da173b5b9b487f8a06b84c9ff6d4d495af95
Milinda Kaushika <milinda.kaushika@scicom.com.my>
2025-09-08 07:37:25 +0530

    add changes

--- src/app/dashboard-coaapplication/dashboard-coaapplication.component.ts
+++ src/app/dashboard-coaapplication/dashboard-coaapplication.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-dashboard-coaapplication',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardCoaapplicationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private iconRegistry: MatIconRegistry,
+              private sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'calendar',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/calendar.svg'));
+  }
 
   ngOnInit(): void {
   }
