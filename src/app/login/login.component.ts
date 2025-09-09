77b7798180b7921f1e49b0a265d3e3d1f2c464ed
Milinda Kaushika <milinda.kaushika@scicom.com.my>
2025-09-09 16:00:35 +0530

    add login img

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +9,18 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,private iconRegistry: MatIconRegistry,
+              private sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'lightning',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/lightning.svg'));
+    iconRegistry.addSvgIcon(
+      'badge',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/badge.svg'));
+    iconRegistry.addSvgIcon(
+      'world',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/world.svg'));
+  }
 
   onLoginClick() {
     this.router.navigate(['/dashboard']);
