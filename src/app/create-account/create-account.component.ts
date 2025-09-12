32348a68fc18609b342453318d2b11cb7b3661e2
Milinda Kaushika <milinda.kaushika@scicom.com.my>
2025-09-12 22:35:29 +0530

    add changes

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
@@ -7,11 +9,34 @@ import { Router } from '@angular/router';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,private iconRegistry: MatIconRegistry,
+              private sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'lightning',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/lightning.svg'));
+    iconRegistry.addSvgIcon(
+      'badge',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/badge.svg'));
+    iconRegistry.addSvgIcon(
+      'world',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/world.svg'));
+    iconRegistry.addSvgIcon(
+      'importer',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/Importer.svg'));
+    iconRegistry.addSvgIcon(
+      'manufacturer',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/manufacturer.svg'));
+    iconRegistry.addSvgIcon(
+      'agent',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/agent.svg'));
+    iconRegistry.addSvgIcon(
+      'right',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/right-arrow.svg'));
+  }
 
   ngOnInit(): void {
   }
-  goToLogin() {
+  goSignIn() {
     this.router.navigate(['/login']);
   }
 }
