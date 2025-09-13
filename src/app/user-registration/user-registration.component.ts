778c9b95f691aca5748509ce4a8f772f14f8a761
Milinda Kaushika <milinda.kaushika@scicom.com.my>
2025-09-13 20:07:40 +0530

    added flag

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Router} from '@angular/router';
+import {COUNTRY_CODES, CountryCode} from '../country-codes';
+
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent implements OnInit {
+  countryCodes: CountryCode[] = COUNTRY_CODES;
+  selectedCountry: CountryCode = this.countryCodes.find(c => c.calling_code === '+94') || this.countryCodes[0];
+  registrationForm: FormGroup;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.registrationForm = new FormGroup({
+      companyName: new FormControl('', Validators.required),
+      businessRegNo: new FormControl('', Validators.required),
+      companyAddress: new FormControl('', Validators.required),
+      contactPersonName: new FormControl('', Validators.required),
+      contactPersonPosition: new FormControl('', Validators.required),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]{7,15}$')
+      ]),
+      emailAddress: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      website: new FormControl('')
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  get fullPhoneNumber(): string {
+    const phone = this.registrationForm.get('phoneNumber')?.value || '';
+    return `${this.selectedCountry.calling_code}${phone}`;
+  }
+
   goToLogin() {
     this.router.navigate(['/login']);
   }
+
 }
