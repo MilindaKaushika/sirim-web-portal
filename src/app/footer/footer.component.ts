22c420cf59273fc9784977b5110701f2d6fa2c66
Milinda Kaushika <milinda.kaushika@scicom.com.my>
2025-09-13 13:27:50 +0530

    add changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,23 +6,6 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  lastScrollTop = 0;
-  showFooterText = true;
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll > this.lastScrollTop) {
-
-      this.showFooterText = true;
-    } else if (currentScroll < this.lastScrollTop) {
-
-      this.showFooterText = false;
-    }
-
-    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Avoid negative scroll
-  }
   constructor() { }
 
   ngOnInit(): void {
